3ae95c59344a1342f9e6f08984b1fa7082deec40
Xavier Carty <xaviercarty@gmail.com>
2019-11-21 18:18:47 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,3 +14,18 @@ WIN_COMBINATIONS = [
         [0,4,8],
         [2,4,6],
 ]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combinations|
+    index = combinations[0]
+    index1 = combinations[1]
+    index2 = combinations[2]
+
+    position1 = board[index]
+    position2 = board[index1]
+    position3 = board[index2]
+
+    if position1 == "X" && position2 == "X" && position3 == "X"
+      return combinations
+    else position1 == "0" && position2 == "0" && position3 == "0"
+      return combinations
