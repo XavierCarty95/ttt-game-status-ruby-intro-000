9306cd6a248645bfa8387dd9c8dad4bd4b220618
Xavier Carty <xaviercarty@gmail.com>
2019-11-21 18:54:51 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -57,9 +57,9 @@ end
 def winner (board)
   index = []
   index = won?(board)
-  if index == false 
+  if index == false
     return nil
-  else 
+  else
     if board[index[0]] == "X"
       return "X"
     else
